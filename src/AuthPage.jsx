4bb04ab6aa2fc062790a8447be62d5c6e8ac1a53
RAPTORH4C3R <ananthunarayanan48@gmail.com>
2023-07-31 20:33:23 +0530

    g

--- src/AuthPage.jsx
+++ src/AuthPage.jsx
@@ -5,7 +5,7 @@ const AuthPage = (props) => {
         e.preventDefault();
         const { value } = e.target[0];
         axios.post(
-            'http://localhost:3001/authenticate',
+            '/authenticate',
             { username: value }
         )
         props.onAuth({ username: value, secret: value })
